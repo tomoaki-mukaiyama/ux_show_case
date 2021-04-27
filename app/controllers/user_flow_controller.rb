94fc5652865b78d39bc004e03eaee96e42d3e7eb
tomoaki-mukaiyama <heymanyaman314@gmail.com>
2021-04-27 10:37:02 +0900

    add tag_top & tag_recommend action

--- app/controllers/user_flow_controller.rb
+++ app/controllers/user_flow_controller.rb
@@ -4,13 +4,31 @@ class UserFlowController < ApplicationController
   end
 
   def tag_index
-    @tags = Tag.all
+    #全タグ取り出して、一つ一つの:nameをjson形式で配列に入れる
+    @tags = Tag.all  
     @tags_array = []
     @tags.each do |tag|
       @tags_array << tag.as_json(only:[:name])
     end
+    render json: { tags: @tags_array }
+  end
+  
+  def tag_top
+    @tags = Tag.where(isTop: 1)
+    @tags_array = []
+    @tags.each do |tag|
+      @tags_array << tag.as_json(only:[:name])
+    end
+    render json: { tags: @tags_array }
+  end
 
-  render json: { tags: @tags_array }
+  def tag_recommend
+    @tags = Tag.where(isRecommend: 1)
+    @tags_array = []
+    @tags.each do |tag|
+      @tags_array << tag.as_json(only:[:name])
+    end
+    render json: { tags: @tags_array }
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'userflow', to: "user_flow#title"
   get 'userflow-tag', to: "user_flow#tag_index"
+  get 'userflow-tag/top', to: "user_flow#tag_top"
+  get 'userflow-tag/recommend', to: "user_flow#tag_recommend"
 end
