c2d47b9a8a197f4233fbaf9041620f4bc9247df3
tomoaki-mukaiyama <heymanyaman314@gmail.com>
2021-04-27 15:35:51 +0900

    change controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
   get "userflow", to: "user_flow#title"
-  
-  get "userflow-tag", to: "tag#tag_index"
-  get "userflow-tag/top", to: "tag#tag_top"
-  get "userflow-tag/recommend", to: "tag#tag_recommend"
 
-  get "screenshot-tag", to: "tag#tag_index"
-  get "screenshot-tag/top", to: "tag#tag_top"
-  get "screenshot-tag/recommend", to: "tag#tag_recommend"
+  get "userflow-tag", to: "user_flow#tag_index"
+  get "userflow-tag/top", to: "user_flow#tag_top"
+  get "userflow-tag/recommend", to: "user_flow#tag_recommend"
+
+  get "screenshot-tag", to: "screenshot#tag_index"
 end
