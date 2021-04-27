5408634ed7f3ad28efac56559769cc55531b6ca7
tomoaki-mukaiyama <heymanyaman314@gmail.com>
2021-04-27 15:47:22 +0900

    edit screenshot_tags action

--- app/controllers/screenshot_controller.rb
+++ app/controllers/screenshot_controller.rb
@@ -3,7 +3,7 @@ class ScreenshotController < ApplicationController
     @screenshot_tags = ScreenShotTag.all
     @tags_array = []
     @screenshot_tags.each do |screenshot_tag|
-      @tags_array << screenshot_tag.as_json(only:[:id, :screen_shot_id, :tag_id])
+      @tags_array << screenshot_tag.as_json(only:[:screen_shot_id, :tag_id])
     end
     render json: {screenshot_tags: @tags_array}
   end
