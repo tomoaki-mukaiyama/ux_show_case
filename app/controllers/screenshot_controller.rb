89cbe91980ffe40ee514fa176b82edd2c7881901
tomoaki-mukaiyama <heymanyaman314@gmail.com>
2021-04-27 17:39:22 +0900

    fix controller

--- app/controllers/screenshot_controller.rb
+++ app/controllers/screenshot_controller.rb
@@ -1,9 +1,9 @@
 class ScreenshotController < ApplicationController
   def tag_index
-    @screenshot_tags = ScreenShotTag.all
+    @screenshot_tags = Tag.where(IsFlowTag: 0)
     @tags_array = []
     @screenshot_tags.each do |screenshot_tag|
-      @tags_array << screenshot_tag.as_json(only:[:screen_shot_id, :tag_id])
+      @tags_array << screenshot_tag.as_json(only:[:name])
     end
     render json: {screenshot_tags: @tags_array}
   end
--- app/controllers/user_flow_controller.rb
+++ app/controllers/user_flow_controller.rb
@@ -5,7 +5,7 @@ class UserFlowController < ApplicationController
 
   def tag_index
     #全タグ取り出して、一つ一つの:nameをjson形式で配列に入れる
-    @tags = Tag.all
+    @tags = Tag.where(IsFlowTag: 1)
     @tags_array = []
     @tags.each do |tag|
       @tags_array << tag.as_json(only:[:name])
