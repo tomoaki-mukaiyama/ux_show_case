650da9b29a02b0a4c74a5fdcf8378b97637c80b2
tomoaki-mukaiyama <heymanyaman314@gmail.com>
2021-06-03 19:06:10 +0900

    destroy screenshots api

--- app/controllers/user_flow_controller.rb
+++ app/controllers/user_flow_controller.rb
@@ -1,44 +1,32 @@
 class UserFlowController < ApplicationController
 
-  #------------tag_typeの値が1のタグ一覧--------------------------#-----------------------------------------------------------
-  
+  #------------タグ一覧--------------------------#-----------------------------------------------------------
   def tag_index
-    # byebug
     if params[:flg] == "recommend"
-      @tags = Tag.where(isRecommend: 1,tag_type: 0)
-      @tags_array = []
-      @tags.each do |tag|
-        @tags_array << tag.as_json
+      tags_array = []
+      Tag.where(isRecommend: 1).each do |tag|
+        tags_array << tag.as_json
       end
-      render json: { tags: @tags_array }
+      render json: { tags: tags_array }
     elsif params[:flg] == "top"
-      @tags = Tag.where(isTop: 1,tag_type: 0)
-      @tags_array = []
-      @tags.each do |tag|
-          @tags_array << tag.as_json
+      tags_array = []
+      Tag.where(isTop: 1).each do |tag|
+        tags_array << tag.as_json
       end
-      render json: { tags: @tags_array }
+      render json: { tags: tags_array }
     else
-      @tags = Tag.where(tag_type: 0)
-      @tags_array = []
-      @tags.each do |tag|
-          @tags_array << tag.as_json
+      tags_array = []
+      Tag.all.each do |tag|
+          tags_array << tag.as_json
       end
-      render json: { tags: @tags_array }
+      render json: { tags: tags_array }
     end
   end
- 
   #------------指定したタグ一件--------------------------#-----------------------------------------------------------
-  def userflow_tag
-
+  def tag
     @tag = Tag.find_by(id: params[:id])
-    if @tag.tag_type == true
-      respond_must_be_userflow
-    else
       render json: { tag: @tag }
-    end
   end
-  
   #------------最新のuserflow(動画)取得----------------#-----------------------------------------------------------
   def latest
     if params[:limit] == 0.to_s       #limit=0を1として扱う
@@ -59,29 +47,25 @@ class UserFlowController < ApplicationController
       
       #全動画を最新順取得＆limit&page指定 => @userflowsに代入
       
-      
       @userflows_array = []
       if params[:tag] #ーーーーーータグ指定ありーーーーーーー
         
-        @userflows = UserFlow.eager_load(:tags,:tag)   #タグ絞り込み　＆　全件取得
+        @userflows = UserFlow.eager_load(:tags)   #タグ絞り込み　＆　全件取得
         .where(tags: {id: params[:tag]})
         .order(created_at: :desc)
         .limit(page_size)
         .offset(page_num * page_size)
         
         @userflows.each do|userflow|
-          hash = UserFlow
+          userflow_hash = UserFlow
           .preload(:tags, :product, :platform)
           .find_by(id: userflow.id)
           .as_json(include: [{product:{only:[:id,:name, :description, :slug, :icon_path]}},{platform:{only:[:id,:name, :slug]}},:tags])     #hash1 所有タグ一覧
-          # byebug
-          maintag = userflow.tag.as_json(root:"maintag")
-          hash = hash.merge(maintag)
 
           if @userflows.count != 1 #userflowが複数ある場合、配列に入れる
-            @userflows_array <<  hash
+            @userflows_array <<  userflow_hash
           else
-            @userflows_array =  hash #一つの場合一つだけ出力
+            @userflows_array =  userflow_hash #一つの場合一つだけ出力
           end
         end
       else            #ーーーーーータグ指定なしーーーーーーー
@@ -91,68 +75,20 @@ class UserFlowController < ApplicationController
         .offset(page_num * page_size)
         
         @userflows.each do|userflow|
-          hash = userflow.as_json(include: [{product:{only:[:id,:name, :description, :slug, :icon_path]}},{platform:{only:[:id,:name, :slug]}},:tags])
-          maintag = userflow.tag.as_json(root:"maintag")
-          hash = hash.merge(maintag)
-
+          userflow_hash = userflow.as_json(include: [{product:{only:[:id,:name, :description, :slug, :icon_path]}},{platform:{only:[:id,:name, :slug]}},:tags])
           if @userflows.count != 1 #userflowが複数ある場合、配列に入れる
-            @userflows_array << hash
+            @userflows_array << userflow_hash
           else
-            @userflows_array = hash #一つの場合一つだけ出力
+            @userflows_array = userflow_hash #一つの場合一つだけ出力
           end
         end
-        
       end
-      
       render json: {userflows: @userflows_array}
-      
-    end
-    
-  end
-  
-  #------------動画詳細ページ取得----------------------------#-----------------------------------------------------------
-  # /userflows/[userflow_id] userflow動画の詳細ページ 最新の１件
-  
-  # .where(products:{name: params[:product]},platforms:{name:params[:platform]},tags: {slug: params[:flowtag]}) 
-  def detail
-    userflow = UserFlow
-    .find_by(id: params[:id])
-    # .preload(:tags, :product, :platform)
-    # byebug
-    if userflow == nil #データが無ければ404notfoundを返す
-      response_not_found #application.rb
-    else
-
-      userflow_main_tag = userflow.tags.find_by(id: userflow.tag_id).as_json(root:"maintag")
-
-      userflow = userflow.as_json(include:[{product:{only:[:id,:name, :description, :slug, :icon_path]}},{platform:{only:[:id,:name, :slug]}},:tags]) 
-      
-      userflow_product_platform_tags_maintag = userflow.merge(userflow_main_tag)
-
-      render json: {userflow: userflow_product_platform_tags_maintag}
-      
     end
   end
-  
-  #------------動画のスクショ一覧取得----------------#-----------------------------------------------------------
-  # /userflows/[userflow_id]/screen_shot 
-  def screenshots
-    @userflow = UserFlow
-    .preload(:tags)
-    .find_by(id: params[:id])
-
-    if @userflow == nil #データが無ければ404notfoundを返す
-      response_not_found #application.rb
-    else
-      @screenshots_with_tags = []
-      @userflow.screen_shots.each do|flow_shot|
-        @screenshots_with_tags << flow_shot.as_json(include: :tags).merge(flow_shot.tag.as_json(root:"maintag"))
-      end 
-      render json: {screenshots: @screenshots_with_tags}
-    end
-  end
+ 
   #------------当該プロダクトの他の動画取得-------------------#-----------------------------------------------------------
-  # /products/[product_id]/ プロダクトの他の動画一覧
+  # /products/[product_id]/userflows プロダクトの他の動画一覧
   def product_userflow
     if params[:limit] == 0.to_s       #limit=0を1として扱う
       page_size = params[:limit].to_i + 1
@@ -176,15 +112,10 @@ class UserFlowController < ApplicationController
       .limit(page_size)
       .offset(page_num * page_size)
       
-      main_tag = []
-      userflows.each do |userflow|
-        main_tag << userflow.tags.find_by(id: userflow.tag_id)
-      end
-      
       array = []
       userflows_tags = userflows.as_json(include:[{product:{only:[:id,:name, :description, :slug, :icon_path]}},{platform:{only:[:id,:name, :slug]}}, :tags])
       userflows_tags.each do |userflow|
-        array << userflow.merge(main_tag[array.count].as_json(root:"maintag"))
+        array << userflow
       end
 
       render json: {userflows: array}
@@ -212,22 +143,17 @@ class UserFlowController < ApplicationController
       .where.not(id: params[:id])
       .limit(page_size)
       .offset(page_num * page_size)
-      # byebug
       userflow_array = []
       userflows.each do |userflow|
         userflow_product_platform = userflow.as_json(include:[{product:{only:[:id,:name, :description, :slug, :icon_path]}},{platform:{only:[:id,:name, :slug]}},:tags])
-        maintag = userflow.tag.as_json(root:"maintag")
-        # byebug
         if userflows.count != 1 #userflowが複数ある場合、配列に入れる
-          userflow_array << userflow_product_platform.merge(maintag)
+          userflow_array << userflow_product_platform
         else
-          userflow_array = userflow_product_platform.merge(maintag) #一つの場合一つだけ出力
+          userflow_array = userflow_product_platform #一つの場合一つだけ出力
         end
       end
-
       render json: {userflows: userflow_array}
     end
-    
   end
   #-----------------------------------------------------------#-----------------------------------------------------------
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,10 @@
 Rails.application.routes.draw do
 
-  get "/userflow_tags", to: "user_flow#tag_index"
-  get "/userflow_tags/:id", to: "user_flow#userflow_tag"
-  
-  get "/screenshot_tags", to: "screenshot#tag_index"
-  get "/screenshot_tags/:id", to: "screenshot#screenshot_tag"
-
-  get "/screenshots" , to: "screenshot#latest"
+  get "/tags", to: "user_flow#tag_index"
+  get "/tags/:id", to: "user_flow#tag"
   
   get "/userflows" , to: "user_flow#latest"
-  get "/userflows/:id" , to: "user_flow#detail"
-  get "/userflows/:id/screenshots" , to: "user_flow#screenshots"
   get "/userflows/:id/recommends" , to: "user_flow#recommended_userflow"
-
+  
   get "/products/:id/userflows" , to: "user_flow#product_userflow"
 end
