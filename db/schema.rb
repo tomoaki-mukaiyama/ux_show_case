edbef6280e1e4347be45019b61d38e804c9c9e4c
tomoaki-mukaiyama <heymanyaman314@gmail.com>
2021-06-04 00:55:41 +0900

    add title to user_flow

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,16 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_074627) do
+ActiveRecord::Schema.define(version: 2021_06_03_155227) do
 
   create_table "platforms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", limit: 255
@@ -47,6 +58,7 @@ ActiveRecord::Schema.define(version: 2021_06_03_074627) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "local_version"
+    t.string "title"
     t.index ["platform_id"], name: "index_user_flows_on_platform_id"
     t.index ["product_id"], name: "index_user_flows_on_product_id"
   end
