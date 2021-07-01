e3e6f636ed94c0357ec03daaed4814e9f7633745
tomoaki_mukaiyama <heymanyaman314@gmail.com>
2021-07-02 01:23:27 +0900

    create 3 new api

--- app/controllers/user_flow_controller.rb
+++ app/controllers/user_flow_controller.rb
@@ -1,6 +1,6 @@
 class UserFlowController < ApplicationController
   before_action :set_pagination_values, only: [:latest, :product_userflow, :recommended_userflow]
-  #------------動画数＆プロダクト数--------------------------#-----------------------------------------------------------
+  #------------スクショ数＆プロダクト数--------------------------#-----------------------------------------------------------
   def stats
     userflow_count = { userflow_count: UserFlow.all.count}
     product_count = { product_count: Product.all.count}
@@ -8,7 +8,7 @@ class UserFlowController < ApplicationController
     render json: { counts: counts }
   end
   #------------タグ一覧--------------------------#-----------------------------------------------------------
-  def tag_index
+  def tags_index
     if params[:flg] == "recommend"
       tags_array = []
       Tag.where(isRecommend: 1).each do |tag|
@@ -39,9 +39,9 @@ class UserFlowController < ApplicationController
       render json: { tag: @tag }
     end
   end
-  #------------最新のuserflow(動画)取得----------------#-----------------------------------------------------------
+  #------------最新のスクショ取得----------------#-----------------------------------------------------------
   def latest
-    #全動画を最新順取得＆limit&page指定 => @userflowsに代入
+    #全スクショを最新順取得＆limit&page指定 => @userflowsに代入
       userflows_array = []
       if params[:tag] #ーーーーーータグ指定ありーーーーーーー
         @userflows = UserFlow.eager_load(:tags)   #タグ絞り込み　＆　全件取得
@@ -49,31 +49,61 @@ class UserFlowController < ApplicationController
         .order(created_at: :desc)
         .limit(@page_size)
         .offset(@page_num * @page_size)
-        # byebug
         @userflows.each do|userflow|
           userflows_array << UserFlow
           .preload(:tags, :product, :platform)
           .find_by(id: userflow.id)
           .as_json(include: [{product:{only:[:id,:name, :description, :slug, :icon_path, :quote_url]}},{platform:{only:[:id,:name, :slug]}},:tags])     #hash1 所有タグ一覧
-
+          
         end
       else            #ーーーーーータグ指定なしーーーーーーー
-        @userflows = UserFlow.preload(:tags, :product, :platform)
+        @userflows = UserFlow
+        .preload(:tags, :product, :platform)
         .order(created_at: :desc)
         .limit(@page_size)
         .offset(@page_num * @page_size)
         
         @userflows.each do|userflow|
-          userflows_array << userflow.as_json(include: [{product:{only:[:id,:name, :description, :slug, :icon_path, :quote_url]}},{platform:{only:[:id,:name, :slug]}},:tags])
-
+          userflows_array << userflow.as_json(include:[{product:{only:[:id,:name, :description, :slug, :icon_path, :quote_url]}},{platform:{only:[:id,:name, :slug]}},:tags])
         end
       end
       render json: {userflows: userflows_array}
+    end
+    
+  #------------当該プロダクトの他のスクショ取得-------------------#-----------------------------------------------------------
+  # /products/[product_id]/userflows プロダクトの他のスクショ一覧
+  def product_userflow
+    userflow_array = []
+    if params[:tag] #ーーーーーータグ指定ありーーーーーーー
+      userflows = UserFlow
+      .eager_load(:product, :platform, :tags)
+      .where(products: {id: params[:id]},tags: {id: params[:tag].split(",")})
+      .limit(@page_size)
+      .offset(@page_num * @page_size)
+        userflows.each do |userflow|
+          userflow_array << UserFlow
+          .preload(:tags, :product, :platform)
+          .find_by(id: userflow.id)
+          .as_json(include: [{product:{only:[:id,:name, :description, :slug, :icon_path, :quote_url]}},{platform:{only:[:id,:name, :slug]}},:tags])     #hash1 所有タグ一覧
+        end
+    else            #ーーーーーータグ指定なしーーーーーーー
+      userflows = UserFlow
+      .eager_load(:product, :platform, :tags)
+      .where(products: {id: params[:id]})
+      .limit(@page_size)
+      .offset(@page_num * @page_size)
+      
+      userflows_tags = userflows.as_json(include:[{product:{only:[:id,:name, :description, :slug, :icon_path, :quote_url]}},{platform:{only:[:id,:name, :slug]}}, :tags])
+      userflows_tags.each do |userflow|
+        userflow_array << userflow
+      end
+    end
+    render json: {userflows: userflow_array}
   end
- 
-    #------------動画詳細ページ取得----------------------------#-----------------------------------------------------------
-  # /userflows/[userflow_id] userflow１件
-  
+
+  #------------スクショ詳細ページ取得----------------------------#-----------------------------------------------------------
+# /userflows/[userflow_id] userflow１件
+
   def detail
     userflow = UserFlow
     .find_by(id: params[:id])
@@ -88,43 +118,61 @@ class UserFlowController < ApplicationController
       
     end
   end
-  
-  #------------当該プロダクトの他の動画取得-------------------#-----------------------------------------------------------
-  # /products/[product_id]/userflows プロダクトの他の動画一覧
-  def product_userflow
-      userflows = UserFlow
-      .eager_load(:product, :platform, :tags)
-      .where(products: {id: params[:id]})
+  #------------関連スクショ取得-------------------#-----------------------------------------------------------
+  def recommended_userflow
+    if UserFlow.find_by(id: params[:id]) == nil
+       return response_not_found
+    else
+      userflows = UserFlow.find_by(id: params[:id])
+      .product.user_flows
+      .where.not(id: params[:id])
       .limit(@page_size)
       .offset(@page_num * @page_size)
-      
       userflow_array = []
-      userflows_tags = userflows.as_json(include:[{product:{only:[:id,:name, :description, :slug, :icon_path, :quote_url]}},{platform:{only:[:id,:name, :slug]}}, :tags])
-      userflows_tags.each do |userflow|
-        userflow_array << userflow
+      userflows.each do |userflow|
+        userflow_array << userflow.as_json(include:[{product:{only:[:id,:name, :description, :slug, :icon_path, :quote_url]}},{platform:{only:[:id,:name, :slug]}},:tags])
       end
       render json: {userflows: userflow_array}
-  end
-  #------------関連動画取得-------------------#-----------------------------------------------------------
-  def recommended_userflow
-      if UserFlow.find_by(id: params[:id]) == nil
-         return response_not_found
-      else
-        userflows = UserFlow.find_by(id: params[:id])
-        .product.user_flows
-        .where.not(id: params[:id])
-        .limit(@page_size)
-        .offset(@page_num * @page_size)
-        userflow_array = []
-        userflows.each do |userflow|
-          userflow_array << userflow.as_json(include:[{product:{only:[:id,:name, :description, :slug, :icon_path, :quote_url]}},{platform:{only:[:id,:name, :slug]}},:tags])
-        end
-        render json: {userflows: userflow_array}
-      end
+    end
   end
   #-----------------------------------------------------------#-----------------------------------------------------------
+  
+    def products_index
+      products = Product.all
+      products_array = []
+      products.each do|product|
+        userflow_count = { userflow_count: product.user_flows.count }
+        product_with_userflow_count = product.as_json.merge(userflow_count)
+        products_array << product_with_userflow_count
+      end
+      render json: {products: products_array}
+    end
+  #-----------------------------------------------------------#-----------------------------------------------------------
+  def product_tags_index
+    userflows = UserFlow
+    .eager_load(:product, :tags)
+    .where(products: {id: params[:id]})
+    
+    tags = []
+    tag_ids_array = []
+    userflows.each do|userflow|
+      tags << userflow.tags
+      tag_ids_array << userflow.tag_ids
+    end
+    tags_hash = tags.flatten.uniq
+    tags_flatten_hash = tag_ids_array.flatten.group_by(&:itself)
+    tags_with_count = []
+    tags_hash.each do |tag|
+      tags_count = { count: tags_flatten_hash[tag.id].count}
+      tags_with_count << tag.as_json.merge!(tags_count)
+    end
+    render json: { product_tags: tags_with_count }
+  end
+  # byebug
+  #-----------------------------------------------------------#-----------------------------------------------------------
+  
   private
-
+  
   def set_pagination_values
     if params[:limit] == 0.to_s       #limit=0を1として扱う
       @page_size = params[:limit].to_i + 1
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,32 @@
 Rails.application.routes.draw do
-
-  get "/stats", to: "user_flow#stats"
-  get "/tags", to: "user_flow#tag_index"
+  get "/tags", to: "user_flow#tags_index"
+  # 1. /tags
+  # 2. /tags?flg=top
+  # 3. /tags?flg=recommend
+  
   get "/tags/:id", to: "user_flow#tag"
+  # 4. /tags/:id
+  
+  get "/userflows/:id" , to: "user_flow#detail"
+  # 5. /userflows/:id
   
   get "/userflows" , to: "user_flow#latest"
-  get "/userflows/:id" , to: "user_flow#detail"
+  # 6. /userflows?page=x&limit=y
+  # 7. /userflows?page=x&limit=y&tag=z
+  
   get "/userflows/:id/recommends" , to: "user_flow#recommended_userflow"
+  # 8. /userflows/:id/recommends?page=x&limit=y
   
   get "/products/:id/userflows" , to: "user_flow#product_userflow"
+  # 9. /products/:id/userflows?page=x&limit=y
+  # 12. /products/:id/userflows?page=x&limit=y&tag=z
+  
+  get "/stats", to: "user_flow#stats"
+  # 10. /stats
+
+  get "/products" , to: "user_flow#products_index"
+  # 11. /products
+
+  get "/products/:id/tags" , to: "user_flow#product_tags_index"
+  # 13. /products/:id/tags
 end
